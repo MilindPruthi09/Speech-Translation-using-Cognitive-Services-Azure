8efe7a94a2f1c1f2427d42ad3b58597019c2dddb
Milind Pruthi <82656184+MilindPruthi09@users.noreply.github.com>
2022-08-20 21:38:28 +0530

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,105 @@
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+import pyaudio
+import wave
+# Import namespaces
+import azure.cognitiveservices.speech as speech_sdk
+from playsound import playsound
+
+def main():
+    try:
+        global speech_config
+        global translation_config
+
+        # Get Configuration Settings
+        load_dotenv()
+        cog_key = os.getenv('COG_SERVICE_KEY')
+        cog_region = os.getenv('COG_SERVICE_REGION')
+
+        # Configure translation
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(cog_key, cog_region)
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        print('Ready to translate from',translation_config.speech_recognition_language)
+
+        # Configure speech
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+
+        # Get user input
+        targetLanguage = ''
+        while targetLanguage != 'quit':
+            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n \n Enter anything else to stop\n').lower()
+            if targetLanguage in translation_config.target_languages:
+                Translate(targetLanguage)
+            else:
+                targetLanguage = 'quit'
+                
+
+    except Exception as ex:
+        print(ex)
+
+def Translate(targetLanguage):
+    translation = ''
+
+    # Translate speech
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
+    
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    audioFile = "output.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(audioFile, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    audioFile='output.wav'
+
+    audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    translator = speech_sdk.translation.TranslationRecognizer(translation_config, audio_config)
+    print("Getting speech from file...")
+    result = translator.recognize_once_async().get()
+    print('Translating "{}"'.format(result.text))
+    translation = result.translations[targetLanguage]
+    print(translation)
+
+    # Synthesize translation
+
+if __name__ == "__main__":
+    main()
